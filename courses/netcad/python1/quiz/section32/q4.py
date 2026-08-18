8e8985db185c15bce5ff194d02a72bb0544d0dbd
Theophilus Jacob <theophilusjacob@gmail.com>
2026-08-18 11:46:28 +0100

    Section quiz 4

---
+++ courses/netcad/python1/quiz/section32/q4.py
@@ -0,0 +1,13 @@
+#Question 4: Create a program with a for loop and a continue statement. 
+#The program should iterate over a string of digits, replace each 0 with x, and print the modified string to the screen. 
+#Use the skeleton below:
+
+for digit in "0165031806510":
+    if digit == "0":
+        print("x", end="")
+        continue
+    print(digit, end="")
+print()
+        # Line of code.
+        # Line of code.
+    # Line of code.
