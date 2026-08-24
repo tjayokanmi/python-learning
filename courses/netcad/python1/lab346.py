4aabbc7829068e5516e80e77df6eb00b8a64e256
Theophilus Jacob <theophilusjacob@gmail.com>
2026-08-24 13:25:17 +0100

    Learning list

--- courses/netcad/python1/lab346.py
+++ courses/netcad/python1/lab346.py
@@ -8,6 +8,9 @@ hat_list[2] = middle_number
 # Step 2: write a line of code that removes the last element from the list.
 del hat_list[-1]
 
+hat_list.append(24)
+hat_list.insert(2, 101)
+
 # Step 3: write a line of code that prints the length of the existing list.
 print(len(hat_list))
 print(hat_list)
