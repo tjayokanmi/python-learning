c2a4a9fa7ae0dfef0ceb297ba283a416cf838c0f
Theophilus Jacob <theophilusjacob@gmail.com>
2026-09-17 14:41:23 +0100

    practice exercise on list: list location

--- courses/netcad/python1/greater_value.py
+++ courses/netcad/python1/greater_value.py
@@ -5,9 +5,14 @@ largest = 0
 for num in range(5):
    number = int(input("Enter a number: "))
    my_list.append(number)
+   #my_list.append(int(input("Enter a number: ")))
 
-for i in range(1, len(my_list)):
-    if my_list[i] > largest:
-        largest = my_list[i]
+# for i in range(1, len(my_list)):
+#     if my_list[i] > largest:
+#         largest = my_list[i]
+
+for i in my_list:
+   if i > largest:
+      largest = i
 
 print("The greater value is: ", str(largest))
